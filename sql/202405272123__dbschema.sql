b3d87079768967c7d259d4fa76e02c936d1c4e01
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-05-27 21:40:50 +0200

    First alterations

--- sql/202405272123__dbschema.sql
+++ sql/202405272123__dbschema.sql
@@ -1,97 +1,76 @@
-
-
 CREATE TABLE "user" (
   "user_id" serial PRIMARY KEY,
-  "uid" char(23),
-  "username" varchar(30)
+  "sub" varchar(2048) UNIQUE NOT NULL,
+  "username" varchar(50) NOT NULL
 );
 
 CREATE TABLE "user_space_role" (
   "user_space_role_id" serial PRIMARY KEY,
-  "user_id" integer,
-  "space_id" integer,
-  "role_id" integer
+  "user_id" integer NOT NULL,
+  "space_id" integer NOT NULL,
+  "role_id" integer NOT NULL
 );
 
-CREATE TABLE "org_admin" (
-  "org_admin_id" serial PRIMARY KEY,
-  "user_id" integer,
-  "org_id" integer
+CREATE TABLE "organization_admin" (
+  "organization_admin_id" serial PRIMARY KEY,
+  "user_id" integer NOT NULL,
+  "organization_id" integer NOT NULL
 );
 
 CREATE TABLE "organization" (
-  "org_id" serial PRIMARY KEY,
-  "org_name" varchar(50) UNIQUE
+  "organization_id" serial PRIMARY KEY,
+  "organization_name" varchar(50) UNIQUE NOT NULL
+  "organization_created" timestamptz NOT NULL,
 );
 
 CREATE TABLE "role" (
   "role_id" serial PRIMARY KEY,
-  "role_name" varchar(25)
+  "role_name" varchar(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE "role_permission" (
-  "role_perm_id" serial PRIMARY KEY,
-  "role_id" integer,
-  "perm_id" integer
+  "role_permission_id" serial PRIMARY KEY,
+  "role_id" integer NOT NULL,
+  "permission_id" integer NOT NULL
 );
 
 CREATE TABLE "permission" (
-  "perm_id" serial PRIMARY KEY,
-  "perm_name" varchar(25)
+  "permission_id" serial PRIMARY KEY,
+  "permission_name" varchar(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE "space" (
   "space_id" serial PRIMARY KEY,
-  "space_name" varchar(50),
-  "org_id" integer
+  "space_name" varchar(50) NOT NULL,
+  "space_created" timestamptz NOT NULL,
+  "organization_id" integer NOT NULL
 );
 
 CREATE TABLE "folder" (
   "folder_id" serial PRIMARY KEY,
-  "folder_name" varchar(25),
-  "space_id" integer
+  "folder_name" varchar(50) NOT NULL,
+  "folder_created" timestamptz NOT NULL,
+  "space_id" integer NOT NULL
 );
 
 CREATE TABLE "page" (
   "page_id" serial PRIMARY KEY,
-  "page_name" varchar(25),
-  "folder_id" integer
+  "page_name" varchar(50) NOT NULL,
+  "page_created" timestamptz NOT NULL,
+  "folder_id" integer NOT NULL
 );
 
 CREATE TABLE "issue" (
   "issue_id" serial PRIMARY KEY,
-  "issue_key" varchar(10),
-  "issue_name" varchar(30),
-  "issue_desc" varchar(100),
-  "page_id" integer,
-  "issue_progress_id" integer
+  "issue_key" varchar(10) NOT NULL,
+  "issue_name" varchar(50) NOT NULL,
+  "issue_description" varchar(500) NOT NULL,
+  "issue_created" timestamptz NOT NULL,
+  "page_id" integer NOT NULL,
+  "issue_progress_id" integer NOT NULL
 );
 
 CREATE TABLE "issue_progress" (
   "issue_progress_id" serial PRIMARY KEY,
-  "issue_progress_name" varchar(15)
+  "issue_progress_name" varchar(50) UNIQUE NOT NULL
 );
-
-ALTER TABLE "issue" ADD FOREIGN KEY ("issue_progress_id") REFERENCES "issue_progress" ("issue_progress_id");
-
-ALTER TABLE "issue" ADD FOREIGN KEY ("page_id") REFERENCES "page" ("page_id");
-
-ALTER TABLE "org_admin" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
-
-ALTER TABLE "user_space_role" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
-
-ALTER TABLE "user_space_role" ADD FOREIGN KEY ("role_id") REFERENCES "role" ("role_id");
-
-ALTER TABLE "user_space_role" ADD FOREIGN KEY ("space_id") REFERENCES "space" ("space_id");
-
-ALTER TABLE "org_admin" ADD FOREIGN KEY ("org_id") REFERENCES "organization" ("org_id");
-
-ALTER TABLE "space" ADD FOREIGN KEY ("org_id") REFERENCES "organization" ("org_id");
-
-ALTER TABLE "folder" ADD FOREIGN KEY ("space_id") REFERENCES "space" ("space_id");
-
-ALTER TABLE "page" ADD FOREIGN KEY ("folder_id") REFERENCES "folder" ("folder_id");
-
-ALTER TABLE "role_permission" ADD FOREIGN KEY ("role_id") REFERENCES "role" ("role_id");
-
-ALTER TABLE "role_permission" ADD FOREIGN KEY ("perm_id") REFERENCES "permission" ("perm_id");
