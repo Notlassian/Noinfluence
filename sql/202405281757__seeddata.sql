a535d5a1fe74ddfe46ef4a6a8cce255ff0386113
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-05-28 18:56:20 +0200

    Update seed data

--- sql/202405281757__seeddata.sql
+++ sql/202405281757__seeddata.sql
@@ -1,26 +1,27 @@
 INSERT INTO issue_progress (issue_progress_name)
-VALUES ("In Progress"), ("Branch Testing"), ("Closed"), ("Ready");
+VALUES ('In Progress'), ('Branch Testing'), ('Closed'), ('Ready');
 
 INSERT INTO permission (permission_name)
-VALUES ("Read"), ("Write"), ("Edit"), ("Delete");
+VALUES ('View'), ('Create'), ('Edit'), ('Delete');
 
 INSERT INTO role (role_name)
-VALUES ("Viewer"), ("Editor"), ("Administrator");
+VALUES ('Viewer'), ('Editor'), ('Administrator');
 
 INSERT INTO role_permission (role_id, permission_id)
 SELECT role.role_id, permission.permission_id
 FROM role, permission
 WHERE role.role_name = 'Viewer';
+AND permission.permission_name IN ('View');
 
 INSERT INTO role_permission (role_id, permission_id)
 SELECT role.role_id, permission.permission_id
 FROM role, permission
 WHERE role.role_name = 'Editor'
-AND permission.permission_name IN ('Read', 'Write', 'Edit');
+AND permission.permission_name IN ('View', 'Edit');
 
 INSERT INTO role_permission (role_id, permission_id)
 SELECT role.role_id, permission.permission_id
 FROM role, permission
 WHERE role.role_name = 'Administrator'
-AND permission.permission_name IN ('Read', 'Write', 'Edit', 'Delete');
+AND permission.permission_name IN ('View', 'Create', 'Edit', 'Delete');
 
