21faeba713e769384cce6c2e0ae12b0722f4276d
Avi <avishkar.motheelal11@gmail.com>
2024-05-29 23:12:07 +0200

    Auth middleware

--- api/app.js
+++ api/app.js
@@ -1,28 +1,32 @@
 import express from 'express';
 import cors from 'cors';
-import { healthCheckRouter } from './Routes/healthRoute.js';
-import { testPermissionRouter } from './Routes/testPermissionsRoute.js';
-const PORT = 8080;
+import {healthCheckRouter} from './Routes/healthRoute.js';
+import {testPermissionRouter} from './Routes/testPermissionsRoute.js';
+import {authenticationMiddleware} from './Middleware/authenticationMiddleware.js';
+
+const port = process.env.PORT || 8080;
+
 export const app = express();
 
-const frontend=process.env.FRONT_END_URL || 'http://localhost:5500';
+const frontendUrl = process.env.FRONT_END_URL || 'http://localhost:5500';
 const corsOptions = {
-    'origin': frontend,
+    'origin': frontendUrl,
     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
     'preflightContinue': false,
     'optionsSuccessStatus': 204
 };
 
 app.use(cors(corsOptions));
+app.use(authenticationMiddleware);
 
 app.use('/healthcheck', healthCheckRouter);
 app.use('/permisssions', testPermissionRouter);
 
-app.listen(PORT, (error) => {
-    if (!error)
-      console.log("Server is Successfully Running, and App is listening on port " + PORT)
-    else
-      console.log("Error occurred, server can't start", error);
-  }
-  );
+app.listen(port, (error) => {
+        if (!error)
+            console.log('Server is Successfully Running, and App is listening on port ' + port);
+        else
+            console.log('Error occurred, server can\'t start', error);
+    }
+);
   
