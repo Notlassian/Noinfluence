8be44e3b464e922514e85cf50aea93b85cb1ff87
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-05-29 18:38:24 +0200

    Naming and fix to create org

--- sql/202405282017__createorgwithadmin.sql
+++ sql/202405282017__createorgwithadmin.sql
@@ -6,16 +6,24 @@ LANGUAGE plpgsql
 AS $$
 DECLARE
     v_user_id INTEGER;
+    v_organization_id INTEGER;
 BEGIN
-    SELECT "user_id" INTO v_user_id
-    FROM "user"
-    WHERE "sub" = p_sub;
+    IF EXISTS (
+        SELECT 1
+        FROM organization
+        WHERE organization_name = p_organization_name
+    ) THEN
+        RAISE EXCEPTION 'Organization name already exists';
+    ELSE
+        SELECT "user_id" INTO v_user_id
+        FROM "user"
+        WHERE "sub" = p_sub;
 
-    INSERT INTO organization (organization_name, organization_created)
-    VALUES (p_organization_name, NOW());
-    
-    INSERT INTO organization_admin ("user_id", "organization_id")
-    SELECT v_user_id, organization_id 
-    FROM organization 
-    WHERE organization_name = p_organization_name;
+        INSERT INTO organization (organization_name, organization_created_at)
+        VALUES (p_organization_name, NOW())
+        RETURNING organization_id INTO v_organization_id;
+        
+        INSERT INTO organization_admin ("user_id", "organization_id")
+        VALUES (v_user_id, v_organization_id);
+    END IF;
 END $$;
