10d94275f43be70e042c7b5416b12302ff9e54b6
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-30 14:35:40 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -24,9 +24,9 @@ const limiter = rateLimit({
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+});
 
-app.use(limiter)
+app.use(limiter);
 // app.use(authenticationMiddleware);
 
 app.use('/', healthCheckRouter);
