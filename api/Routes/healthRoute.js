7fc14a91d6985c3c72b3739b9943c70e9d81d94d
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-30 19:47:27 +0200

    added auth route

--- api/Routes/healthRoute.js
+++ api/Routes/healthRoute.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { getHealth } from '../Controllers/healthCheckController.js';
+import { authenticationMiddleware } from '../Middleware/authenticationMiddleware.js';
 export const healthCheckRouter = Router();
 
 healthCheckRouter.get('/', getHealth);
+healthCheckRouter.get('/auth', authenticationMiddleware, getHealth);
