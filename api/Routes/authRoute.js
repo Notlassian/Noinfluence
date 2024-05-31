a03d277a6a31eb914751e8c0210ded9d3e40aafe
ryanbasiltrickett <ryanbasiltrickett@gmail.com>
2024-05-31 22:42:52 +0200

    Added perm middleware

--- api/Routes/authRoute.js
+++ api/Routes/authRoute.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { checkAuthed, postToken } from '../Controllers/tokenController.js';
+import { authenticationMiddleware } from '../Middleware/authenticationMiddleware.js';
 
 export const authRouter = Router();
 
 authRouter.post('/token', postToken);
-authRouter.get('/check', checkAuthed);
+authRouter.get('/check', authenticationMiddleware, checkAuthed);
--- api/Routes/spaceRoute.js
+++ api/Routes/spaceRoute.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { spaceAdminRouter } from './spaceAdminRoute';
+import {
+    hasSpacePermission,
+    isOrgAdmin
+} from '../Middleware/authorizationMiddleware.js';
 
 export const spaceRouter = Router();
 
-spaceRouter.use('/:spaceName/admin', spaceAdminRouter);
+spaceRouter.use('/:spaceName/admin', hasSpacePermission("edit users"), spaceAdminRouter);
 
-// spaceRouter.post('/add', postToken);
+// spaceRouter.post('/add', isOrgAdmin, postToken);
 // spaceRouter.get('/list', checkAuthed);
