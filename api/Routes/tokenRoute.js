0cbffce65c616f4eb32b84a446066366d8df31cc
ryanbasiltrickett <ryanbasiltrickett@gmail.com>
2024-05-31 10:26:42 +0200

    Fixed import error

--- api/Routes/tokenRoute.js
+++ api/Routes/tokenRoute.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getHealth } from '../Controllers/healthCheckController.js';
-import { postToken } from '../controllers/tokenController.js';
+import { postToken } from '../Controllers/tokenController.js';
 
 export const tokenRouter = Router();
 
