721d8bd74a063f29860a14c3971cd4be149b62d2
Ryan Trickett <64923970+ryanbasiltrickett@users.noreply.github.com>
2024-06-01 22:07:54 +0200

    Revert "Feature/kan 80"

--- api/Controllers/organisationsController.js
+++ api/Controllers/organisationsController.js
@@ -1,5 +1,4 @@
 import { sqlPool } from '../Utils/dbUtils.js';
-import { buildUniqueMap } from '../Utils/mapUtils.js';
 
 export const createOrg = async (req, res) => {
     var query = 'call create_organization_and_admin($1,$2)';
@@ -25,20 +24,13 @@ export const createOrg = async (req, res) => {
 
 export const getMyOrgs = async (req, res) => {
     var query =
-        'Select organization_name, space_name FROM user_space_organization_permissions where username=$1';
+        'Select DISTINCT (organization_name, space_name) FROM user_space_organization_permissions where username=$1';
     var params = [req.user];
     if (!params[0]) res.status(500).json({ error: 'Internal Server Error' });
     else {
         sqlPool
             .query(query, params)
-            .then((sqlRes) => {
-                const resMap = buildUniqueMap(
-                    sqlRes.rows,
-                    'organization_name',
-                    'space_name'
-                );
-                res.status(200).json(resMap);
-            })
+            .then((sqlRes) => res.status(200).json(sqlRes.rows))
             .catch((error) => {
                 console.log(error);
                 res.status(500).json({ error: 'Internal Server Error' });
--- api/Controllers/spaceController.js
+++ api/Controllers/spaceController.js
@@ -42,4 +42,4 @@ export const getFoldersWithPages = async (req, res) => {
                 });
             });
     }
-};
+}
