e4f1a2cd954cdea9220038de1ed639cad0ffea23
ryanbasiltrickett <ryanbasiltrickett@gmail.com>
2024-06-01 10:40:44 +0200

    Removed temp view

--- sql/V202406010944__views.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW organization_admin_view AS
-SELECT organization_admin_id, username, organization_name
-FROM "user" 
-INNER JOIN "organization_admin" ON "user".user_id = "organization_admin".user_id
-INNER JOIN "organization" ON "organization_admin".organization_id = "organization".organization_id
