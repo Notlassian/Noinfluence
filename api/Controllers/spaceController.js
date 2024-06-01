78bbdedd229d776f99333ac7b14d038ae6199197
ryanbasiltrickett <ryanbasiltrickett@gmail.com>
2024-06-01 22:10:11 +0200

    Added get folders and pages

--- api/Controllers/spaceController.js
+++ api/Controllers/spaceController.js
@@ -1,4 +1,5 @@
 import { sqlPool } from '../Utils/dbUtils.js';
+import { buildUniqueMap } from '../Utils/mapUtils.js';
 
 export const createSpace = async (req, res) => {
     var query = 'call insert_space($1,$2)';
@@ -23,22 +24,25 @@ export const createSpace = async (req, res) => {
 };
 
 export const getFoldersWithPages = async (req, res) => {
-    var query = 'SELECT folder';
-    var params = [req.body.Space, req.params.orgName];
+    var query = 'SELECT folder_name, page_name from space_pages_view WHERE organization_name = $1 AND space_name = $2';
+    var params = [req.params.orgName, req.params.spaceName];
     if (!params[0] || !params[1])
         res.status(500).json({ error: 'Internal Server Error' });
     else {
         sqlPool
             .query(query, params)
-            .then(() =>
-                res.status(200).json({
-                    message: `${params[0]} in ${params[1]} has been created successfully`,
-                })
-            )
+            .then(() => {
+                const resMap = buildUniqueMap(
+                    sqlRes.rows,
+                    'folder_name',
+                    'page_name'
+                );
+                res.status(200).json(resMap);
+            })
             .catch((error) => {
                 console.log(error);
                 res.status(500).json({
-                    error: `${params[0]} in ${params[1]} already exists`,
+                    error: `Couldn't access pages for ${params[0]}, ${params[1]}`,
                 });
             });
     }
