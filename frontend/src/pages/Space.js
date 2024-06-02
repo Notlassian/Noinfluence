ecab87a7370d3d0fda784a8d6e7ae83220e56187
ryanbasiltrickett <ryanbasiltrickett@gmail.com>
2024-06-02 22:17:20 +0200

    Removed unused vars

--- frontend/src/pages/Space.js
+++ frontend/src/pages/Space.js
@@ -1,12 +1,7 @@
-import { SpaceDashboard, SpaceSideNavigationBar } from "../components";
+import { SpaceSideNavigationBar } from "../components";
 import './css/Space.css';
 
 export const Space = () => {
-
-  const description = 'This space is to demostrate';
-  const openIssues = ['Issue 1', 'Issue 2', 'Issue 3'];
-  const openIssuesNum = 4;
-  const completedIssuesNum = 7;
 
   const sideNavBarItem = [
     ['page1','./dot.png'],
