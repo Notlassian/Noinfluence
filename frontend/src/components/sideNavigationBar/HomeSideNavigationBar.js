e33feb5e7ced8085346a4ed1c92fde2141a3ce87
ryanbasiltrickett <ryanbasiltrickett@gmail.com>
2024-06-06 00:19:27 +0200

    Made hyperlinks red

--- frontend/src/components/sideNavigationBar/HomeSideNavigationBar.js
+++ frontend/src/components/sideNavigationBar/HomeSideNavigationBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CreateSpacePopUp, CreateOrganisationPopUp } from '../popup';
-import { AlertType, HttpStatusCodes, getData, postData, postDataWithoutBearer, showAlert } from '../../utils';
+import { AlertType, HttpStatusCodes, getData, postDataWithoutBearer, showAlert } from '../../utils';
 import '../css/HomeSideNavigationBar.css';
 
 export const HomeSideNavigationBar = () => {
